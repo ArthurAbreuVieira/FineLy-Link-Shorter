21ae6a89fc3381740f30bb9ca7a2b2ecb10c4565
Arthur Abreu Vieira Mendes <arthurabreuvieiramendes@gmail.com>
2021-09-02 21:03:37 -0300

    Enviado dados dinamicamente para os modais de editar e deletar na view details.html

--- public/js/updateLink.js
+++ public/js/updateLink.js
@@ -1,6 +1,12 @@
 import httpFetch from 'http://localhost/likn/public/js/httpFetch.js';
 import fragments from 'http://localhost/likn/public/js/fragments.js';
 
+const link = {
+  id: document.getElementById('linkId') ? document.getElementById('linkId').innerText : null,
+  redirect: document.getElementById('linkRedirect') ? document.getElementById('linkRedirect').innerText : null,
+  clickCount: document.getElementById('clickCount') ? document.getElementById('clickCount').innerText : null
+}
+
 const actionsBtn = document.querySelectorAll("[data-btn=actions]");
 actionsBtn.forEach(btn => {
   btn.addEventListener('click', () => {
@@ -9,7 +15,10 @@ actionsBtn.forEach(btn => {
       modal = fragments.modal;
     } else {
       modal = fragments.modalDelete;
+      modal = modal.replaceAll('{{link.click_count}}', link.clickCount);
     }
+    modal = modal.replaceAll('{{link.redirect}}', link.redirect);
+    modal = modal.replaceAll('{{link.id}}', link.id);
 
     const fragment = document.createRange().createContextualFragment(modal).firstElementChild;
     document.body.insertAdjacentElement("afterbegin", fragment);
